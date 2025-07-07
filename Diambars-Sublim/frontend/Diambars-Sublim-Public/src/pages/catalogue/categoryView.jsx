8c49db70218f30f75341a153e2d247d81cba3589
santiago <santiago4v5@gmail.com>
2025-07-07 15:24:27 -0600

    Seguimiento del catálogo

--- Diambars-Sublim/frontend/Diambars-Sublim-Public/src/pages/catalogue/categoryView.jsx
+++ Diambars-Sublim/frontend/Diambars-Sublim-Public/src/pages/catalogue/categoryView.jsx
@@ -6,15 +6,22 @@ import './categoryView.css';
 
 const dummyProducts = [
   { name: 'De vestir', image: '/images/products/vestir.png' },
+  { name: 'Clásica', image: '/images/products/clasica.png' },
+  { name: 'Manga Corta', image: '/images/products/manga-corta.png' },
   { name: 'Cuello V', image: '/images/products/cuello-v.png' },
-  // etc...
+  { name: 'Manga Larga', image: '/images/products/manga-larga.png' },
+  { name: 'Sin Mangas', image: '/images/products/sin-mangas.png' },
+  { name: 'Polo', image: '/images/products/polo.png' },
+  { name: 'Deportiva', image: '/images/products/deportiva.png' },
+  { name: 'Cuello Mao', image: '/images/products/cuello-mao.png' },
+
 ];
 
 export default function CategoryView() {
   const { categoria } = useParams();
 
   return (
-    <>
+    <div className='category-container'>
       <div className="category-page">
         <SidebarFilters />
         <div className="product-grid">
@@ -29,6 +36,6 @@ export default function CategoryView() {
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
