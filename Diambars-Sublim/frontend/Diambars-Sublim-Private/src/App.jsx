89ca0bf23318f53908f20ea0d7a8a847d03c0b71
STVEEEN <cuper.fcb@gmail.com>
2025-07-14 15:02:47 -0600

    arreglos de categorias

--- Diambars-Sublim/frontend/Diambars-Sublim-Private/src/App.jsx
+++ Diambars-Sublim/frontend/Diambars-Sublim-Private/src/App.jsx
@@ -48,11 +48,13 @@ import CodeConfirmationPage from './page/codeConfirmationPage/codeConfirmation';
 import NewPasswordPage from './page/newPasswordPage/newPassword';
 import WelcomePage from './page/welcomePage/welcome';
 import CatalogManagementPage from './page/catalogManagementPage/catalogManagement';
-import { CategoryProvider } from './context/categoryContext/categoryContext'; // ✅ Importación añadida
+import { CategoryProvider } from './context/categoryContext/categoryContext'; // ✅ Solo el contexto
+import CategoryPage from './page/categoryPage/category'; // ✅ Página separada
 import ProductCreation from './page/productCreationPage/ProductCreation';
 
 function App() {
   return (
+    <CategoryProvider>
     <AuthProvider>
       <Router>
         <Routes>
@@ -71,6 +73,7 @@ function App() {
         </Routes>
       </Router>
     </AuthProvider>
+    </CategoryProvider>
   );
 }
 
